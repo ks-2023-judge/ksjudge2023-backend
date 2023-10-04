50c1a40a97294c755467bcd641a93d496ee13dbf
admin <1226hajing@gmail.com>
2023-10-04 12:15:47 +0900

    주석취소

--- src/app.js
+++ src/app.js
@@ -34,29 +34,29 @@ app.use(
   })
 );
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
-// app.use('/graphql', (req, res, next) => {
-//   // GraphQL 요청에 query 필드가 없는 경우, 요청 그대로 전달
-//   if (!req.body || !req.body.query) {
-//     return next();
-//   }
+app.use("/graphql", (req, res, next) => {
+  // GraphQL 요청에 query 필드가 없는 경우, 요청 그대로 전달
+  if (!req.body || !req.body.query) {
+    return next();
+  }
 
-//   // GraphQL 쿼리가 login query가 아닌 경우
-//   if (!req.body.query.includes('login')) {
-//     // req.session에서 stud_id와 학번 확인
-//     const studId = req.session.studId;
-//     const studNo = req.session.studNo;
+  // GraphQL 쿼리가 login query가 아닌 경우
+  if (!req.body.query.includes("login")) {
+    // req.session에서 stud_id와 학번 확인
+    const studId = req.session.studId;
+    const studNo = req.session.studNo;
 
-//     // stud_id와 학번이 없는 경우 권한이 없다는 응답 반환
-//     if (!studId || !studNo) {
-//       return res.status(403).json({ message: '권한이 없습니다.' });
-//     }
-//   }
+    // stud_id와 학번이 없는 경우 권한이 없다는 응답 반환
+    if (!studId || !studNo) {
+      return res.status(403).json({ message: "권한이 없습니다." });
+    }
+  }
 
-//   // 권한이 있는 경우 다음 미들웨어로 이동
-//   next();
-// });
+  // 권한이 있는 경우 다음 미들웨어로 이동
+  next();
+});
 
 app.use(
   "/graphql",
